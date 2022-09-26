e1ecf5aa16816bae0134eeab97128aefea70e489
lecty kim <jpg-png@naver.com>
2022-09-26 10:44:12 +0900

    First commit spring boot test commit

---
+++ sreview/src/main/java/com/sreviewonly/board/SreviewApplication.java
@@ -0,0 +1,13 @@
+package com.sreviewonly.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SreviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SreviewApplication.class, args);
+    }
+
+}
---
+++ sreview/src/test/java/com/sreviewonly/board/SreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sreviewonly.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
