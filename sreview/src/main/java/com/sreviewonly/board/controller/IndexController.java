11e3098ab765a54751bd425f1a72d0e836264839
Lecty Kim <jpg-png@naver.com>
2022-09-28 02:11:13 +0900

    feat: view단 경로 전체 수정

---
+++ sreview/src/main/java/com/sreviewonly/board/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.sreviewonly.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "index",method = RequestMethod.GET)
+    public String goIndex(HttpServletRequest request){
+        return "index";
+    }
+}
