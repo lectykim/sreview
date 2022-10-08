9a4da52df7c82e17455c696300de3a1f8362a5ff
Lecty Kim <jpg-png@naver.com>
2022-10-08 10:21:30 +0900

    feat: Thymeleaf View 연결 완료. 이후 Controller을 짜면 됨.

--- sreview/src/main/java/com/sreviewonly/board/controller/IndexController.java
+++ sreview/src/main/java/com/sreviewonly/board/controller/IndexController.java
@@ -12,13 +12,60 @@ public class IndexController {
 
     //특정 mapping에 도착하면, return값으로 templates의 절대 경로를 돌려줌.
 
-    @RequestMapping(value = "index/signin",method = RequestMethod.GET)
+    @RequestMapping(value = "signin",method = RequestMethod.GET)
     public String goSignin(HttpServletRequest request){
         return "signin";
     }
 
+    @RequestMapping(value = "sign_in_part_1",method = RequestMethod.GET)
+    public String goSigninPart1(HttpServletRequest request){
+        return "sign_in_part_1";
+    }
+
+    @RequestMapping(value = "review_find_by_product",method = RequestMethod.GET)
+    public String goReviewProduct(HttpServletRequest request){
+        return "review_find_by_product";
+    }
+
+    @RequestMapping(value = "review_find_by_user",method = RequestMethod.GET)
+    public String goReviewUser(HttpServletRequest request){
+        return "review_find_by_user";
+    }
+
+    @RequestMapping(value = "review_view",method = RequestMethod.GET)
+    public String goReviewView(HttpServletRequest request){
+        return "review_view";
+    }
+
+    @RequestMapping(value = "review_write_page",method = RequestMethod.GET)
+    public String goReviewWritePage(HttpServletRequest request){
+        return "review_write_page";
+    }
+
+    @RequestMapping(value = "congratulation",method = RequestMethod.GET)
+    public String goCongratulation(HttpServletRequest request){
+        return "congratulation";
+    }
+
+    @RequestMapping(value = "counsel_page",method = RequestMethod.GET)
+    public String goCounsel(HttpServletRequest request){
+        return "counsel_page";
+    }
+
+
+    @RequestMapping(value = "counsel_management_page",method = RequestMethod.GET)
+    public String goCounselManage(HttpServletRequest request){
+        return "counsel_management_page";
+    }
+
+
     @RequestMapping(value = "index",method = RequestMethod.GET)
     public String goIndex(HttpServletRequest request){
+        return "index";
+    }
+
+    @RequestMapping(value = "",method = RequestMethod.GET)
+    public String goAll(HttpServletRequest request){
         return "index";
     }
 
